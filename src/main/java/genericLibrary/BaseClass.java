26dcf67ad49720e40f4233c45db47383d6e6146a
HAVN <HAVN@ESKW190662.esko-graphics.com>
2023-09-07 20:35:51 +0530

    child commit

--- src/main/java/genericLibrary/BaseClass.java
+++ src/main/java/genericLibrary/BaseClass.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
---
+++ src/test/java/Scripts/Demo.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+
+public void demo()
+{
+	System.out.println("Hi Demo");
+}
+}
